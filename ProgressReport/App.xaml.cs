2e4b7f7b8f7cdc9b24c65c4129b5c026046ca747
ryanchurchill <ryan.churchill@gmail.com>
2023-05-11 23:57:55 -0700

    Add project files.

---
+++ ProgressReport/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgressReport;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ProgressReport/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgressReport;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
